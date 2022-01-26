b434d3eb0ff9df09a517f984e0f361e289570055
kevin mulinari kuhn <kevinkuhn@MB-004171.local>
2022-01-25 22:21:51 -0300

    Initial commit

---
+++ primeiro-app/primeiro_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  primeiro_appApp.swift
+//  primeiro-app
+//
+//  Created by kevin mulinari kuhn on 25/01/22.
+//
+
+import SwiftUI
+
+@main
+struct primeiro_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
